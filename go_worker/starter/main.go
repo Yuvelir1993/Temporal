431511af84f2377ffdce4226f9a7cc3f9d2f6bc5
plozovik <plozovikov@gmail.com>
2023-06-16 09:24:44 +0200

    GO worker

---
+++ go_worker/starter/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"context"
+	"github.com/google/uuid"
+	helloworld "go-worker"
+	"log"
+
+	"go.temporal.io/sdk/client"
+)
+
+func main() {
+	// The client is a heavyweight object that should be created once per process.
+	c, err := client.Dial(client.Options{})
+	if err != nil {
+		log.Fatalln("Unable to create client", err)
+	}
+	defer c.Close()
+
+	uuid4, nil := uuid.NewUUID()
+
+	workflowOptions := client.StartWorkflowOptions{
+		ID:        "go-" + uuid4.String(),
+		TaskQueue: "go-worker-task-queue",
+	}
+
+	we, err := c.ExecuteWorkflow(context.Background(), workflowOptions, helloworld.Workflow, "Temporal")
+	if err != nil {
+		log.Fatalln("Unable to execute workflow", err)
+	}
+
+	log.Println("Started workflow", "WorkflowID", we.GetID(), "RunID", we.GetRunID())
+
+	// Synchronously wait for the workflow completion.
+	var result string
+	err = we.Get(context.Background(), &result)
+	if err != nil {
+		log.Fatalln("Unable get workflow result", err)
+	}
+	log.Println("Workflow result:", result)
+}
